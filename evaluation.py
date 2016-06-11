420219ff9f21aa3773c8ed979b9e51593d98cff6
i.samoilenko <i.samoilenko@corp.nekki.ru>
2016-06-12 01:01:28 +0300

    золотой стандарт болванка v1

--- evaluation.py
+++ evaluation.py
@@ -1,7 +1,13 @@
-import csv
+from datamanager import data_spewer, save_csv
+from hashtags import Analyzer
 
-def gold_standard_template():
-    pass
+def gold_standard_template(cut=1000):
+    analyzer = Analyzer()
+    dataListOfLists = []
+    for id, originalString in data_spewer(lambda x: True if not "#" in x else False):
+        hashtags, onlytext = analyzer.tweet_hashtags(originalString, True)
+        dataListOfLists.append([originalString, onlytext, " ".join(hashtags.keys())]*2)
+    save_csv("gold_template", dataListOfLists[:cut], ["originalString", "onlytext", "hashtags", "goldString", "goldonlytext", "goldhashtags"])
 
 if __name__ == "__main__":
-    pass
+    gold_standard_template()
