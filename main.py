72bcf12dc3051356e117f801f272694237372e20
i.samoilenko <i.samoilenko@corp.nekki.ru>
2016-06-12 19:04:48 +0300

    post_process в целом будет таким

--- main.py
+++ main.py
@@ -11,10 +11,16 @@ analyzer = Analyzer()
 и к каждому хэштегу поле refinedHT"""
 def post_process(tweetsList):
     for tweet in tweetsList:
-        '''получаем обработанну строку без хэштегов и со старыми хэштегами (чтобы их заменить на новые в конце'''
-        refinedOnlyText, preRefinedOriginalString = text_normalize(tweet['originalString'],
-                                                                   tweet['hashtags'].keys())
-
+        '''Проходимся по хэштегам'''
+        hashtagsOldNew = {}
+        for hashtag in tweet['hashtags']:
+            htDic = tweet['hashtags'][hashtag]
+            htDic['refinedHT'] = segmentation(hashtag, htDic)
+            hashtagsOldNew[hashtag] = htDic['refinedHT']
+        '''получаем обработанну строку без хэштегов и со старыми хэштегами (чтобы их заменить на новые в конце hashtagsOldNew = OrderedDict(sorted(dic.items(), key=lambda t: len(t[0])))'''
+        tweet['refinedOnlyText'], tweet['refinedString'] = text_normalize(tweet['originalString'], hashtagsOldNew)
+    with open("data/output/db_RESULT.json", "w", encoding="utf-8") as file_output:
+        file_output.write(json.dumps(tweetsList, sort_keys=True, ensure_ascii=False, indent=4, separators=(',', ': ')))
 
 def json_all_dump():
     tweetsList = []
@@ -35,15 +41,16 @@ def json_load():
     return tweetsList
 
 if __name__ == "__main__":
-    json_all_dump()
-    dic = defaultdict(int)
-    for tweet in json_load():
-        hashtagsDic = tweet['hashtags']
-        for ht in hashtagsDic:
-            if hashtagsDic[ht]['viewType'] == 'CamelCase':
-                print(ht)
-                dic[ht] += 1
-
-    dic = OrderedDict(OrderedDict(sorted(dic.items(), key=lambda t: t[1])))
-    for i in dic:
-        print(i, dic[i])
+    # json_all_dump()
+    # dic = defaultdict(int)
+    # for tweet in json_load():
+    #     hashtagsDic = tweet['hashtags']
+    #     for ht in hashtagsDic:
+    #         if hashtagsDic[ht]['viewType'] == 'CamelCase':
+    #             print(ht)
+    #             dic[ht] += 1
+    #
+    # dic = OrderedDict(sorted(dic.items(), key=lambda t: t[1]))
+    # for i in dic:
+    #     print(i, dic[i])
+    post_process(json_load())
